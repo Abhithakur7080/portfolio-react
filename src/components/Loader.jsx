08d5a84c55b8d82c7aa48343c1e43cbbd8e56d0d
Abhijeet Kumar <abhijeetthakur7080@gmail.com>
2024-12-07 17:42:28 +0530

    education & experience updated

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,6 @@
 import { Stack, Typography } from "@mui/material";
 import React from "react";
+import bgImage from "../assets/bgImage.jpeg"
 
 const Loader = () => {
   return (
@@ -9,7 +10,7 @@ const Loader = () => {
       sx={{
         marginTop: { xs: "54px", sm: "64px" },
         background:
-          "url(https://img.freepik.com/free-vector/elegant-dark-background-with-golden-details_23-2148438266.jpg?t=st=1733389291~exp=1733392891~hmac=31fc71da2d1e881d142042e770cc60c7b0f6b3ccb2e7e13c5a35d873b1699e03&w=1060)",
+          `url(${bgImage})`,
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
         backgroundPosition: "center",
